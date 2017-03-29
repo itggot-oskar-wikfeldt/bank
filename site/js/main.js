1b76bf6d70d72386e3770f2605840dd39e91e2f3
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-03-29 12:34:59 +0200

    saker och ting

--- site/js/main.js
+++ site/js/main.js
@@ -3,7 +3,7 @@ var saldo = 50;
 function loan() {
     var x = parseInt(document.getElementById("loanInput").value);
     document.getElementById("loanInput").value = "";
-    if (!isNaN(x)) {
+    if (!isNaN(x) && x > 0) {
         saldo += x;
         document.getElementById("saldo").innerHTML = "Saldo: " + saldo + "kr";
     }
