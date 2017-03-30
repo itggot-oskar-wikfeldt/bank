e9af46e38af23e76c9ef451c0c34e05c6e800a27
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-03-30 15:04:59 +0200

    jsjas

--- site/js/main.js
+++ site/js/main.js
@@ -6,6 +6,9 @@ function loan() {
     if (!isNaN(x) && x > 0) {
         saldo += x;
         document.getElementById("saldo").innerHTML = "Saldo: " + saldo + "kr";
+        console.log("adaaam");
+        var success = document.querySelector("success");
+        success.classList.remove("hidden");
     }
 }
 
@@ -15,9 +18,9 @@ function openOverview() {
 }
 
 document.getElementById("loanInput")
-.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        document.getElementById("loanButton").click();
-    }
-});
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("loanButton").click();
+        }
+    });
